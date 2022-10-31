c5446df923c394961c618472f1f00f8c2a2f7dbc
MansiBora11 <boramansi118@gmail.com>
2022-10-31 20:06:16 +0530

    Master branch

--- app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
+++ app/src/main/java/com/example/gitpracticeyt/MainActivity.kt
@@ -15,6 +15,6 @@ class MainActivity :
             R.layout.activity_main
         )
         println("THIS IS CONTAINED IN MY SECOND COMMIT")
-        today is a good day haha
+        println("Master branch")
     }
 }
